50e538b0d73c32638e65b94aab73649088a5231c
jiin-coder <fpahstiq123@naver.com>
2022-04-02 03:41:53 +0900

    User 엔티티 추가

---
+++ src/main/java/com/kja/webPrj/user/domain/User.java
@@ -0,0 +1,15 @@
+package com.kja.webPrj.user.domain;
+
+
+import javax.persistence.Entity;
+import java.time.LocalDateTime;
+
+@Entity
+public class User {
+    public long id;
+    public LocalDateTime regDate;
+    public LocalDateTime updateDate;
+    public String email;
+    public String password;
+    public String name;
+}
